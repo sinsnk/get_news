0193c0309875bfebbb903fdf497722841670973d
sinsnk <cb.so.devope@gmail.com>
2015-08-01 17:12:10 +0900

    Fix URL & cannot search by 0 searchCnt

--- lib/get_news.rb
+++ lib/get_news.rb
@@ -4,8 +4,12 @@ require "open-uri"
 
 module GetNews
   class Main
-    def get_news(search_word, news_count) 
-      url = 'http://news.google.com/news/?ie=UTF8&output=atom&hl=ja&num=' + news_count + '&q=' + search_word
+    def get_news(search_word, news_count)
+      if news_count < 1
+        return
+      end
+      
+      url = 'https://news.google.com/news?ned=us&ie=UTF-8&oe=UTF-8&q=' + search_word + '&output=atom&num=' + news_count.to_i + '&hl=ja'
 
       charset = nil
       xml = open(url) do |f|
