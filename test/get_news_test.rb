64c4c00235cf2d6f7934df58cd33f48faf0bbdea
kinchan33 <a1520mk@aiit.ac.jp>
2015-08-01 18:24:42 +0900

    mod test

--- test/get_news_test.rb
+++ test/get_news_test.rb
@@ -14,17 +14,17 @@ class GetNewsTest < Minitest::Test
 
   def test_get_news_check_news_count
     articles = @main.get_news("Git", 3)
-    assert_true articles.length <= 3
+    assert articles.length <= 3
   end
 
   def test_get_news_check_first_line
-    articles = @main.get_news("おもてなし", 5)
-    assert_true articles[0].length > 0 
+    articles = @main.get_news("", 5)
+    assert articles.length > 0 
   end
 
   def test_get_news_check_news_count_zero
     articles = @main.get_news("Git", 0)
-    assert_nil artcles
+    assert_nil articles
   end
 
 end
