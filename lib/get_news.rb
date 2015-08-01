d28a3015f00b2faee0fcb7e62621772f7e2f1e82
vagrant <vagrant@enpit>
2015-08-01 06:51:28 +0000

    update git_news.rb

--- lib/get_news.rb
+++ lib/get_news.rb
@@ -1,7 +1,21 @@
 require "get_news/version"
+require "nokogiri"
+require "open-uri"
 
 module GetNews
   class Main
-    
+    def get_news(search_word, news_count) 
+      url = 'http://news.google.com/news/?hl=ja&num=' + news_count + '&q=' + search_word
+
+      charset = nil
+      html = open(url) do |f|
+        charset = f.charset
+        f.read
+      end
+
+      doc = Nokogiri::HTML.parse(html, nil, charset)
+
+      p doc.title
+    end
   end
 end
