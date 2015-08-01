f6f0bd692b77bee334236dfb5885135e643431f5
kinchan33 <a1520mk@aiit.ac.jp>
2015-08-01 16:30:26 +0900

    add test code.

--- test/get_news_test.rb
+++ test/get_news_test.rb
@@ -1,11 +1,24 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
+require 'get_news'
 
 class GetNewsTest < Minitest::Test
+
+  def setup
+    @main = GetNews::Main.new
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::GetNews::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_get_news_check_news_count
+    articles = @main.getNews("Git", 3)
+    assert_equal 3, articles.length
+  end
+
+  def test_get_news_check_first_line
+    articles = @main.getNews("おもてなし", 5)
+    assert_equal true,  articles[0].length > 0 
   end
 end
